f554762ef6dec698330476613bc84af162eec266
unknown <Saakh@DESKTOP-2NU7BDI.com>
2024-04-24 13:39:18 +0300

    finished

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,19 @@ require('./gulp/dev.js');
 require('./gulp/docs.js');
 
 gulp.task(
-	'default',
-	gulp.series(
-		'clean:dev',
-		gulp.parallel('html:dev', 'sass:dev', 'images:dev', 'fonts:dev', 'files:dev', 'js:dev'),
-		gulp.parallel('server:dev', 'watch:dev')
-	)
+    'default',
+    gulp.series(
+        'clean:dev',
+        gulp.parallel('html:dev', 'sass:dev', 'images:dev', 'fonts:dev', 'files:dev', 'js:dev'),
+        'watch:dev' // Убираем server:dev из default
+    )
 );
 
 gulp.task(
-	'docs',
-	gulp.series(
-		'clean:docs',
-		gulp.parallel('html:docs', 'sass:docs', 'images:docs', 'fonts:docs', 'files:docs', 'js:docs'),
-		gulp.parallel('server:docs')
-	)
+    'docs',
+    gulp.series(
+        'clean:docs',
+        gulp.parallel('html:docs', 'sass:docs', 'images:docs', 'fonts:docs', 'files:docs', 'js:docs'),
+        gulp.parallel('server:docs')
+    )
 );
